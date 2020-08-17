4e418c44a2184037d408f1a6e55bed7460a95b06
Patrick Wees <patrick.wees@anedot.com>
2020-08-17 00:24:48 -0700

    Update variable names within hook to match docs

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-import { useEffect, useRef, useState } from "react";
-import throttle from "lodash.throttle";
+import { useEffect, useRef, useState } from "react"
+import throttle from "lodash.throttle"
 
-// Drop in replacement for useState with deepMerge built in
-// const [val, setVal] = useThrottledState(defaultValue, timeout, dispatchFunc)
-// val and setVal update immediately locally, but do not push updates
-// until dispatch runs
-const useThrottledState = (defaultVal, timeout, dispatch) => {
-  const [value, setValue] = useState(defaultVal);
+const useThrottledState = (defaultVal, throttleRate, dispatch) => {
+  const [value, setValue] = useState(defaultVal)
 
-  const throttledFunc = useRef(throttle(dispatch, timeout)).current;
+  const throttledFunc = useRef(throttle(dispatch, throttleRate)).current
 
   useEffect(() => {
-    throttledFunc(value);
-  }, [value]);
-  return [value, setValue];
-};
+    throttledFunc(value)
+  }, [value])
 
-export default useThrottledState;
+  return [value, setValue]
+}
+
+export default useThrottledState
